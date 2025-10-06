04c13c3e9151ab39a379e3de3f6c9852b1c05746
AnnisaPutriAnanda24si <annisa24si@mahasiswa.pcr.ac.id>
2025-10-06 09:53:01 +0700

    Penambahan Volt_template

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -47,3 +48,5 @@ Route::get('/pegawai',[PegawaiController::class,'index']);
 
 Route::post('question/store', [QuestionController::class, 'store'])
 		->name('question.store');
+
+Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
